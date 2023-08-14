76cdd6ad2590aec70f62ada21d615e145df5ea80
shubh-tripath-i <shubh14201tripathi@gmail.com>
2023-08-15 00:54:04 +0530

    Setup mysql database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DJANGO_MYSQL_DB'],
+        'CONN_MAX_AGE': 1800,
+        'USER': os.environ['DJANGO_MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_DB_PASSWORD'],
+        'HOST': os.environ['DJANGO_MYSQL_HOST'],
+        'PORT': os.environ['DJANGO_MYSQL_PORT']
     }
 }
 
