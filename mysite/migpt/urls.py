8348085ee312e40d0121c10782de07f74b6bb4fc
shubh-tripath-i <shubh14201tripathi@gmail.com>
2023-09-06 00:11:28 +0530

    Fixed path url

--- mysite/migpt/urls.py
+++ mysite/migpt/urls.py
@@ -5,11 +5,11 @@ app_name = 'migpt'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_question/', views.get_question, name="get_question"),
     path('accounts/signup/', views.signup, name='signup'),
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit', views.update_profile, name='update_profile'),
-    re_path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.verify_email, name='verify_email'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.verify_email, name='verify_email'),
+    path('get_question/', views.get_question, name="get_question"),
     path('create-interview-session/', views.create_interview_session, name='create_interview_session'),
     path('start-interview/', views.start_interview, name='start_interview'),
     path('save-answer/', views.save_answer, name='save_answer')
--- mysite/migpt/utils.py
+++ mysite/migpt/utils.py
@@ -1,4 +1,4 @@
-# from langchain.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
@@ -18,17 +18,12 @@ def feed_data():
 def prompt_generator(interview):
     # Will generate prompt based on given user input
 
-    #     system_template = """You are an interviewer. You are conducting an interview for role of {job_role}
-    #  for company {company_name}. Generate {number_of_questions} questions regarding it to test if the
-    #  candidate is a valid fit. {question_generation_instructions} Use the following questions asked in
-    #  the past to generate new questions.\n_______________\n{context}"""
+    # template = "You are an interviewer. "
 
-    template = "You are an interviewer. "
+    # if interview.company:
+    #     template += "You work for {company}. "
 
-    if interview.company:
-        template += "You work for {company}. "
-
-    template += "You are hiring for role of {job_role}. "
+    # template += "You are hiring for role of {job_role}. "
 
     # if interview.job_description:
     #     template+="The job description is: {job_description}. "
@@ -45,22 +40,28 @@ def prompt_generator(interview):
     # if interview.tools:
     #     template+="You should test candidate's knowledge on following tools: {tools}"
 
-    template += "The difficulty level of questions should be {difficulty_level}. "
+    # template += "The difficulty level of questions should be {difficulty_level}. "
 
     # if interview.user_instructions:
     #     template+="Further instructions are: {user_instructions}."
 
-    template += "Include a variety of questions so they can check both theoretical and practical\
- knowledge of the candidate. The questions asked should be in a sequence like ask all questions\
- belonging to the same domain and then jump to the next domain and do the same to judge the candidate\
- topic and domain wise. The following are the questions and answers asked in past. You can\
- either use the same questions or modify them to generate new questions.\
- Also, give the answers with it."
-    template += "\n____________\n{context}"
+#     template += "Include a variety of questions so they can check both theoretical and practical\
+#  knowledge of the candidate. The questions asked should be in a sequence like ask all questions\
+#  belonging to the same domain and then jump to the next domain and do the same to judge the candidate\
+#  topic and domain wise. The following are the questions and answers asked in past. You can\
+#  either use the same questions or modify them to generate new questions.\
+#  Also, give the answers with it."
+#     template += "\n____________\n{context}"
+
+    template = """You are an interviewer. You are conducting an interview for role of {job_role}.
+ Generate {number_of_questions} questions regarding it to test if the candidate is a valid fit.
+\n{question_generation_instructions}\n The following are the questions and
+ answers asked in past. You can either use the same questions or modify them to generate new questions.
+ Also, give the answers with it.\n_______________\n{context}"""
 
     prompt = SystemMessagePromptTemplate(
             prompt=PromptTemplate(
-                input_variables=[],
+                input_variables=['job_role', 'number_of_questions', 'question_generation_instructions', 'context'],
                 template=template, template_format='f-string', validate_template=True
             ), additional_kwargs={}
         )
@@ -68,10 +69,14 @@ def prompt_generator(interview):
 
 
 def generate_questions(interview):
-    # prompt  = prompt_generator(interview)
-    # chain = make_chain(prompt)
-    demo_questions = ["Tell me about yourself", "What is DSA?"]
-    return demo_questions
+    return ["Tell me about yourself", "What is DSA?"]
+    prompt = prompt_generator(interview)
+    chain = make_chain(prompt)
+    question_generation_instructions = ""
+    response = chain({'job_role': "Software Developer",
+                      'number_of_questions': "10",
+                      'question_generation_instructions': question_generation_instructions })
+    print(response)
 
 
 def make_chain(prompt):
@@ -80,17 +85,16 @@ def make_chain(prompt):
         temperature="0",
     )
 
-    vector_db = Milvus(
+    vector_db = Chroma(
         collection_name="interview-data",
         embedding_function=OpenAIEmbeddings(),
-        # connection_args={"host": "127.0.0.1", "port": "19530"},
     )
 
     chain = ConversationalRetrievalChain.from_llm(
         model,
         retriever=vector_db.as_retriever(),
         return_source_documents=False,
-        verbose=False,
+        verbose=True,
         combine_docs_chain_kwargs=dict(prompt=prompt),
         rephrase_question=False,
         return_generated_question=False,
