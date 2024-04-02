63d472c1dab0187cdec80c4b26abd5eef68a1e1f
shubh-tripath-i <shubh14201tripathi@gmail.com>
2024-04-02 13:54:11 +0530

    Fixed Debug value

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = bool(os.environ.get('DEBUG', False))
 
 if DEBUG:
     HOST = 'http://127.0.0.1:8000/'
